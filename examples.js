15da24c9ec378b6e0698487de9b470e430c2cbbb
unknown <PriyaS@WI-CCP-ST-04.wilkescc.edu>
2022-04-10 14:27:49 -0400

    completed

--- examples.js
+++ examples.js
@@ -1,27 +1,27 @@
-let myArray = [3,5,6,9,12]
+let myArray = [3, 5, 6, 9, 12]
 
-let transformedArray = myArray.map((number)=>{
+let transformedArray = myArray.map((number) => {
     return number * 2
 })
-// console.log(transformedArray)
+console.log(transformedArray)
 
-let evens = myArray.filter((number)=>{
-    return number %2 == 0
+let evens = myArray.filter((number) => {
+    return number % 2 == 0
 })
 // console.log(evens)
 
 
-let flattenArray = myArray.reduce((acc, cur)=>{
+let flattenArray = myArray.reduce((acc, cur) => {
     // console.log(acc, cur)
     return acc + cur
 })
 // console.log(flattenArray)
 
-function reducer(array){
+function reducer(array) {
     //write a function that takes an array of numbers and returns the sum of all numbers 
     let sum = 0
-    for(let i=0;i<array.length;i++){
-        sum += array[i] 
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]
     }
     return sum
 }
@@ -36,11 +36,73 @@ console.log(triple)
 let filterEvenTriple = triple.filter(number => number % 2 == 0)
 console.log(filterEvenTriple)
 
-let reduceTriple = filterEvenTriple.reduce((acc, cur)=> acc + cur)
+let reduceTriple = filterEvenTriple.reduce((acc, cur) => acc + cur)
 // console.log(reduceTriple)
 
 let chain = myArray.map(number => number * 3)
-.filter(number => number % 2 == 0)
-.reduce((acc, cur) => acc + cur)
+    .filter(number => number % 2 == 0)
+    .reduce((acc, cur) => acc + cur)
 
 console.log(chain)
+
+const transformersMap = () => {
+    let transformers = [
+        {
+            name: 'Optimus Prime',
+            form: 'Freightliner Truck',
+            team: 'Autobot'
+        },
+        {
+            name: 'Megatron',
+            form: 'Gun',
+            team: 'Decepticon'
+        },
+        {
+            name: 'Bumblebee',
+            form: 'VW Beetle',
+            team: 'Autobot'
+        },
+        {
+            name: 'Soundwave',
+            form: 'Walkman',
+            team: 'Decepticon'
+        }
+    ];
+    const forms = transformers.map((transformer) => transformer.form)
+    return forms;
+}
+
+const reduceTransformers = transformers.reduce((acc, cur) => {
+
+    if (acc[cur.team]) {
+        acc[cur.team] = acc[cur.team] + 1
+    }
+    else {
+        acc[cur.team] = 1
+    }
+    return acc;
+
+}, {})
+console.log(reduceTransformers)
+
+const inputs = [1, -4, 12, 0, -3, 29, -150]
+let sum = 0
+const positiveNumber = inputs.filter((input) => {
+    if (input >= 0) {
+        return input;
+    }
+    else {
+        return 0;
+    }
+})
+//console.log(positiveNumber)
+const sumofNumbers = positiveNumber.reduce((acc, cur) => {
+    return acc + cur
+})
+//console.log(sumofNumbers)
+const nameInitials = () => {
+    const input = 'George Raymond Richard Martin';
+    inits = input.split(" ").map(word => word[0]).join("")
+    console.log(inits)
+}
+nameInitials()
--- test.js
+++ test.js
@@ -3,30 +3,71 @@ Map Transformers
 Using map, return an array of each transformer's value of its 'form' property 
 */
 //result: ['Freightliner Truck','Gun', 'VW Beetle','Walkman']
+
+let transformers = [
+  {
+    name: 'Optimus Prime',
+    form: 'Freightliner Truck',
+    team: 'Autobot'
+  },
+  {
+    name: 'Megatron',
+    form: 'Gun',
+    team: 'Decepticon'
+  },
+  {
+    name: 'Bumblebee',
+    form: 'VW Beetle',
+    team: 'Autobot'
+  },
+  {
+    name: 'Soundwave',
+    form: 'Walkman',
+    team: 'Decepticon'
+  }
+];
+
+//implicit return
+const transformerMap = transformers.map(transformer => transformer.form)
+// console.log(transformerMap)
+
+// without implicit
+const transformerMap1 = transformers.map((transformer) => {
+  return (transformer.form)
+})
+// console.log(transformerMap1)
+
+//within function.
 const transformersMap = () => {
-    let transformers = [
-        {
-          name: 'Optimus Prime',
-          form: 'Freightliner Truck',
-          team: 'Autobot'
-        },
-        {
-          name: 'Megatron',
-          form: 'Gun',
-          team: 'Decepticon'
-        },
-        {
-          name: 'Bumblebee',
-          form: 'VW Beetle',
-          team: 'Autobot'
-        },
-        {
-          name: 'Soundwave',
-          form: 'Walkman',
-          team: 'Decepticon'
-        }
-      ];
+  let transformers = [
+    {
+      name: 'Optimus Prime',
+      form: 'Freightliner Truck',
+      team: 'Autobot'
+    },
+    {
+      name: 'Megatron',
+      form: 'Gun',
+      team: 'Decepticon'
+    },
+    {
+      name: 'Bumblebee',
+      form: 'VW Beetle',
+      team: 'Autobot'
+    },
+    {
+      name: 'Soundwave',
+      form: 'Walkman',
+      team: 'Decepticon'
+    }
+  ];
+  const forms = transformers.map((transformer) => transformer.form)
+  console.log(forms)
 }
+transformersMap()
+
+
+
 /*
 Filter Transformers
 Using filter, return an array of transformer objects that have the 'team' property of 'Autobot'
@@ -44,31 +85,35 @@ result: [ {
     ]
 
 */
-const transformersFilter = () => {
-    let transformers = [
-        {
-          name: 'Optimus Prime',
-          form: 'Freightliner Truck',
-          team: 'Autobot'
-        },
-        {
-          name: 'Megatron',
-          form: 'Gun',
-          team: 'Decepticon'
-        },
-        {
-          name: 'Bumblebee',
-          form: 'VW Beetle',
-          team: 'Autobot'
-        },
-        {
-          name: 'Soundwave',
-          form: 'Walkman',
-          team: 'Decepticon'
-        }
-      ]
 
+const transformersFilter = () => {
+  let transformers = [
+    {
+      name: 'Optimus Prime',
+      form: 'Freightliner Truck',
+      team: 'Autobot'
+    },
+    {
+      name: 'Megatron',
+      form: 'Gun',
+      team: 'Decepticon'
+    },
+    {
+      name: 'Bumblebee',
+      form: 'VW Beetle',
+      team: 'Autobot'
+    },
+    {
+      name: 'Soundwave',
+      form: 'Walkman',
+      team: 'Decepticon'
+    }
+  ]
+  const transfilter = transformers.filter((transformer) => transformer.team === 'Autobot')
+  return transfilter
 }
+console.log(transformersFilter())
+
 
 /*
 Reduce Transformers
@@ -78,31 +123,43 @@ result: {
     Decepticon: 2
 }
 */
-const reduceTransformers = () => {
-    let transformers = [
-        {
-          name: 'Optimus Prime',
-          form: 'Freightliner Truck',
-          team: 'Autobot'
-        },
-        {
-          name: 'Megatron',
-          form: 'Gun',
-          team: 'Decepticon'
-        },
-        {
-          name: 'Bumblebee',
-          form: 'VW Beetle',
-          team: 'Autobot'
-        },
-        {
-          name: 'Soundwave',
-          form: 'Walkman',
-          team: 'Decepticon'
-        }
-      ]
-}
 
+
+const reduceTransformers = () => {
+  let transformers = [
+    {
+      name: 'Optimus Prime',
+      form: 'Freightliner Truck',
+      team: 'Autobot'
+    },
+    {
+      name: 'Megatron',
+      form: 'Gun',
+      team: 'Decepticon'
+    },
+    {
+      name: 'Bumblebee',
+      form: 'VW Beetle',
+      team: 'Autobot'
+    },
+    {
+      name: 'Soundwave',
+      form: 'Walkman',
+      team: 'Decepticon'
+    }
+  ]
+  const reducetransf = transformers.reduce((acc, curr) => {
+    if (acc[curr.team]) {
+      acc[curr.team] = acc[curr.team] + 1
+    }
+    else {
+      acc[curr.team] = 1
+    }
+    return acc
+  }, {})
+  console.log(reducetransf)
+}
+reduceTransformers();
 
 
 /*
@@ -113,22 +170,58 @@ Use filter and reduce
 */
 
 //result: 42
+
 const sumPositiveElement = () => {
-    const input = [ 1, -4, 12, 0, -3, 29, -150]
+  const input = [1, -4, 12, 0, -3, 29, -150]
+
+  const positiveNumber = input.filter((number) => {
+    return number > 0
+  })
+  console.log(positiveNumber)
+  const sumofNumbers = positiveNumber.reduce((acc, curr) => {
+    return acc + curr;
+  })
+  console.log(sumofNumbers)
 }
-
-
+sumPositiveElement()
 
 /* 
-Calculate median and mean
-Calculate the mean and median values of the number elements from the input array.
-
-Use: reduce, sort, Math.abs
+Calculate median 
+Calculate the mean value of the number elements from the input array.
+Use: reduce, sort, 
 */
-//result: { mean: 38.5, median: 32 }
-const medianMean = () => {
-    const input = [12, 46, 32, 64]
+//result: 38.5
+
+const mean = () => {
+  const input = [12, 46, 32, 64]
+  const addinput = input.reduce((acc, curr) => {
+    return acc + curr
+  })
+  console.log(addinput)
+  const meanofarray = addinput / input.length;
+  console.log(meanofarray)
 }
+mean()
+
+//BONUS - calculate mean and median and return as object: { mean: 38.5, median: 39 } use Math.abs
+const medianMean = () => {
+  const input = [12, 46, 32, 64]
+  const sortedarray = input.sort()
+  console.log(sortedarray)
+  let halfway = sortedarray.length / 2
+  let oddway = (sortedarray.length + 1) / 2
+  if (halfway % 2 === 0) {
+    const median = (sortedarray[halfway - 1] + sortedarray[halfway]) / 2
+    console.log(median)
+  }
+  else {
+    oddmedian = sortedarray[oddway - 1]
+    console.log(oddmedian)
+  }
+
+}
+medianMean()
+
 
 
 /*
@@ -138,8 +231,11 @@ Use .map , .split , .join
 */
 //result: 'GRRM'
 const nameInitials = () => {
-    const input = 'George Raymond Richard Martin';
+  const input = 'George Raymond Richard Martin';
+  inits = input.split(" ").map(word => word[0]).join('')
+  console.log(inits)
 }
+nameInitials()
 
 /*
 Age difference from the youngest and oldest
@@ -149,27 +245,36 @@ Use .map , Math.min , Math.max
 
 //Result: [13, 67, 54]
 const ageDifference = () => {
-    const input = [
-        {
-          name: 'John',
-          age: 13
-        },
-        {
-          name: 'Mark',
-          age: 56,
-        },
-        {
-          name: 'Rachel',
-          age: 45,
-        },
-        {
-          name: 'Nate',
-          age: 67,
-        },
-        {
-          name: 'Jeniffer',
-          age: 65,
-        }
-      ];
+  const input = [
+    {
+      name: 'John',
+      age: 13
+    },
+    {
+      name: 'Mark',
+      age: 56,
+    },
+    {
+      name: 'Rachel',
+      age: 45,
+    },
+    {
+      name: 'Nate',
+      age: 67,
+    },
+    {
+      name: 'Jeniffer',
+      age: 65,
+    }
+  ];
+  const ages = input.map((inpu) => inpu.age)
+  console.log(ages)
+  const oldest = Math.max(...ages)
+  // console.log(oldest)
+  const youngest = Math.min(...ages)
+  return [youngest, oldest, (oldest - youngest)]
 }
+console.log(ageDifference())
+
+
 
